1eda20ce00b95abf3d7ace4326c987fc555fe863
Fungi360 <aidan@knut.com>
2024-11-09 13:43:26 -0500

    Connecting QT and GitHub Via Git GUI First Attempts

---
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_NovaMindMaster_FILE C:/Users/aidan/OneDrive/Documents/FGCU/Fall 2024/Programming 2/NovaMind/NovaMindMaster/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/NovaMindMaster.exe)
+set(__QT_DEPLOY_TARGET_NovaMindMaster_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_NovaMindMaster_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
